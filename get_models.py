f4c829baa4996d953e2d7202de7652cef2b0c1f5
Tesfa Yohannes <tesfa1993@yahoo.com>
2018-06-04 12:40:02 +0300

    Check for file existance'

--- get_models.py
+++ get_models.py
@@ -1,6 +1,7 @@
 import requests
 import bz2
 import math
+import os
 from tqdm import tqdm
 
 model = "http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2"
@@ -12,4 +13,8 @@ decompressor = bz2.BZ2Decompressor()
 with open('shape_predictor_68_face_landmarks.dat', 'wb') as f:
     for data in tqdm(response.iter_content(1024), total=math.ceil(total_size//1024), unit='KB'):
         f.write(decompressor.decompress(data))
-        print('Does this')
+if ('shape_predictor_68_face_landmarks.dat' in os.listdir()):
+    # TODO use checksum
+    print('Model persumably downloaded')
+else:
+    print('Model isnot available')
